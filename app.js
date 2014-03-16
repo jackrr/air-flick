d85e1cfd69f4bf9e71b242c447aab5935f0f5c72
jackrr <jackratner@gmail.com>
2014-03-16 11:24:53 -0400

    add useful modules, initializer files

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/scripts/main.js
@@ -0,0 +1,5 @@
+
+var dust = require('dustjs-linkedin');
+var $ = require('jquery')(window);
+var Backbone = require('backbone');
+Backbone.$ = $;
