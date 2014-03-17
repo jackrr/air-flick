791147ef16e338631d4f2ede1f45efb2b238ebf2
Nathan <nteetor@oberlin.edu>
2014-03-17 10:16:06 -0400

    Added app directory and blank xcode project

---
+++ app/AirFlick/AirFlick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AirFlick
+//
+//  Created by Nathan Teetor on 3/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ app/AirFlick/AirFlick/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirFlick
+//
+//  Created by Nathan Teetor on 3/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
